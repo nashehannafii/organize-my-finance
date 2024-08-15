bd73826b7f88284cb1954af839d7c5d356108420
nashehannafii <nashehannafii@gmail.com>
2024-08-16 02:36:14 +0700

    [Nasheh] init

---
+++ web/themes/yii2-adminlte3/layouts/content.php
@@ -0,0 +1,75 @@
+<?php
+/* @var $content string */
+
+use yii\bootstrap4\Breadcrumbs;
+?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">
+                        <?php
+                        if (!is_null($this->title)) {
+                            echo \yii\helpers\Html::encode($this->title);
+                        } else {
+                            echo \yii\helpers\Inflector::camelize($this->context->id);
+                        }
+                        ?>
+                    </h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <?php
+                    echo Breadcrumbs::widget([
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                        'options' => [
+                            'class' => 'breadcrumb float-sm-right'
+                        ]
+                    ]);
+                    ?>
+                </div><!-- /.col -->
+            </div>
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+
+    <div class="content-header">
+        <div class="container-fluid">
+            <?php if (Yii::$app->user->isGuest) : ?>
+                <?= $content ?>
+            <?php endif; ?>
+            <?php if (!Yii::$app->user->isGuest) : ?>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">
+                                    <?php
+                                    if (!is_null($this->title)) {
+                                        echo \yii\helpers\Html::encode($this->title);
+                                    } else {
+                                        echo \yii\helpers\Inflector::camelize($this->context->id);
+                                    }
+                                    ?>
+                                </h3>
+                            </div>
+
+                            <div class="card-body">
+                                <?= $content ?>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+            <?php endif; ?>
+        </div><!-- /.container-fluid -->
+    </div>
+
+</div>
